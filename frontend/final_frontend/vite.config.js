0ee3d05feae0126bd5dc37f55a254518b228d118
Chirag <chiragdevadiga252@gmail.com>
2026-08-13 03:26:02 +0530

    Update Vite proxy configuration

--- frontend/final_frontend/vite.config.js
+++ frontend/final_frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'https://task-manager-525.onrender.com',
         changeOrigin: true,
       },
     },
